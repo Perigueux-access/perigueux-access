49ce118a8d5ff1fa035990df7a4e5049c5aa3c9b
Perigueux-access <perigueux.access@gmail.com>
2026-04-19 20:39:22 +0200

    Refactor rendering of PerigueuxAccess component

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client'
 import PerigueuxAccess from './PerigueuxAccess.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PerigueuxAccess />
-  </React.StrictMode>
+  React.createElement(PerigueuxAccess)
 )
