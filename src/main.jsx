3f27105e217ab41e5f9319ca72421b42a38ee1a2
Perigueux-access <perigueux.access@gmail.com>
2026-04-19 12:43:53 +0200

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PerigueuxAccess from './PerigueuxAccess.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PerigueuxAccess />
+  </React.StrictMode>
+)
